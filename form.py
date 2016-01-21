473b29458fd76829633f3d555d3694bf915de2c9
Mike Birdsall <m.g.birdsall@mbirdsall.com>
2016-01-21 13:21:20 -0500

    Major changes to architecture

--- form.py
+++ form.py
@@ -1,154 +1,214 @@
 #!/usr/bin/python
+"""
+Program to load information about what I eat from an Android phone
+
+This cgi-bin program is one of a group of programs to help me create and
+maintain a record of everything I eat. This one writes an html form page
+out and checks to see if it has been submitted with values. If it has (and
+possible is validated) it will write out the data to files on the server.
+
+Other programs will run to process the input data and prepare output web pages
+detailing what I ate. There will be other cgi-bin programs to edit and manage
+the data in various forms.
+
+"""
 import cgi
 import cgitb; cgitb.enable()
 import os, sys
+from ConfigParser import SafeConfigParser
+from datetime import datetime
 
 UPLOAD_DIR = "/big/dom/xkirkbird/www/and/test"
 
-def text_out(message):
-    print """ <p>%s</p>
-    </body></html>
-    """ % (message,)
-    sys.exit()
+class main():
+    def __init__(self):
+        self.fileitem = None
+        self.status = ""
+        self.picfile_name = None
 
-print """Content-Type: text/html\n
-<head>
-    <meta name="viewport" content="width=320" />
-<style>
-form {
-    width:300px;
-}
+        self.head()
+        self.body()
+        self.tail()
 
-label {
-    display: inline-block;
-    text-align:left;
-}
+    def tail(self):
+        print """    <p>%s</p>\n    </body>\n</html>
+        """ % (self.status,)
 
-label.nutrit {
-    width:130px;
-    text-align:right;
-}
+    def text_out(self, message):
+        self.status = message
 
-input.nutrit {
-    display:inline-block;
-    width:70px;
-}
+    def head(self):
+        print """Content-Type: text/html\n\n<html>\n    <head>
+        <meta name="viewport" content="width=320" />\n"""
+        self.css()
+        print """ </head>
+            """
 
-label.inst {
-    width:70px;
-    text-align:right;
-}
+    def css(self):
+        print """        <style>
+            form {
+                width:300px;
+            }
 
-input:inst {
-    text-align:left;
-}
+            label {
+                display: inline-block;
+                text-align:left;
+            }
 
-input {
-    display:inline-block;
-    text-align:right;
-}
-fieldset {
-    background:#fff7db;
-}
+            label.nutrit {
+                width:130px;
+                text-align:right;
+            }
 
-</style>
-</head>
+            input.nutrit {
+                display:inline-block;
+                width:70px;
+            }
 
-<body>
+            label.inst {
+                width:70px;
+                text-align:right;
+            }
 
-<h1>Food Entry</h1>
-<form method="post" enctype="multipart/form-data" action="http://kirkbird.com/cgi-bin/food/form.py">
+            input:inst {
+                text-align:left;
+            }
 
-  <input type="submit"><br>
+            input {
+                display:inline-block;
+                text-align:right;
+            }
+            fieldset {
+                background:#fff7db;
+            }
+        </style>"""
 
-  <fieldset style="width:270px">
-  <legend>Image Entry:</legend>
-  <input type="file" name="pic" accept="image/*"/><br>
-  </fieldset>
+    def body(self):
+        print """<body>"""
 
-  <fieldset style="width:270px">
-    <legend>Identifying Information:</legend>
+        self.print_form()
+        self.parse_form_fields()
 
-  Description: (used as title for dish)<br> 
-    <input type="text" name="description" placeholder="Title/Description/Identifier"/>
-    <br>
-  Comment:<br><input type="text" name="comment" placeholder="Comment/Context/Excuse">
-    <br>
-    Amount:<br> <input type="text" name="size" placeholder="Like 2 cups or 12 oz or large bowl">
-  </fieldset>
+    def print_form(self):
+        print """    <h1>Food Entry</h1>\n    <form method="post" enctype="multipart/form-data" action="http://kirkbird.com/cgi-bin/food/form.py">
+        <input type="submit"><br>
 
-  <fieldset style="width:270px">
-  <legend>Nutrition:</legend>
-  <label class="nutrit" for="calories">Calories:</label>
-  <input class="nutrit" type="number" name="calories" id="calories" max="3000">
-  <label class="nutrit" for="carbs">Carbs(g):</label>
-  <input class="nutrit" type="number" name="carbs" id="carbs" size="2" max="300"><br>
-  <label class="nutrit" for="prot">Protein(g):</label>
-  <input class="nutrit" type="number" name="protein" id="prot" size="2" max="300"><br>
-  <label class="nutrit" for="fat">Fat(g):</label>
-  <input class="nutrit" type="number" name="fat" id="fat" size="2" max="300">
-  </fieldset>
+        <fieldset style="width:270px">
+        <legend>Image Entry:</legend>
+        <input type="file" name="pic" accept="image/*"/><br>
+        </fieldset>
 
-  <fieldset style="width:270px">
-  <legend>Instance Information:</legend>
-  <label class="inst" for="servings">Servings:</label>
-  <input class="inst" type="number" name="servings" id="servings" min="1" max="9" value="1"><br>
+        <fieldset style="width:270px">
+            <legend>Identifying Information:</legend>
 
-  <label class="inst" for="day">Day:</label>
-  <input class="inst" type="date" name="day" id="day"><br>
+        Description: (used as title for dish)<br> 
+            <input type="text" name="description" placeholder="Title/Description/Identifier"/>
+            <br>
+        Comment:<br><input type="text" name="comment" placeholder="Comment/Context/Excuse">
+            <br>
+            Amount:<br> <input type="text" name="size" placeholder="Like 2 cups or 12 oz or large bowl">
+        </fieldset>
 
-  <label class="inst" for="time">Time:</label>
-  <input type="time" name="time"><br>
+        <fieldset style="width:270px">
+        <legend>Nutrition:</legend>
+        <label class="nutrit" for="calories">Calories:</label>
+        <input class="nutrit" type="number" name="calories" id="calories" max="3000">
+        <label class="nutrit" for="carbs">Carbs(g):</label>
+        <input class="nutrit" type="number" name="carbs" id="carbs" size="2" max="300"><br>
+        <label class="nutrit" for="prot">Protein(g):</label>
+        <input class="nutrit" type="number" name="protein" id="prot" size="2" max="300"><br>
+        <label class="nutrit" for="fat">Fat(g):</label>
+        <input class="nutrit" type="number" name="fat" id="fat" size="2" max="300">
+        </fieldset>
 
-  <label class="inst" for="meal">Meal:</label>
-  <input class="inst" list="meals" id="meal" name="meal">
-    <datalist id="meals">
-      <option value="Breakfast">
-      <option value="Lunch">
-      <option value="Supper">
-      <option value="Snack">
-    </datalist>
-  <br>
-  </fieldset>
-  <input type="submit"><br>
-</form>
-"""
+        <fieldset style="width:270px">
+        <legend>Instance Information:</legend>
+        <label class="inst" for="servings">Servings:</label>
+        <input class="inst" type="number" name="servings" id="servings" min="1" max="9" value="1"><br>
 
-  # Meal Selections should be changed to be based on table
-  # Meal selections could be radio buttons (perhaps too hard on phone
+        <label class="inst" for="day">Day:</label>
+        <input class="inst" type="date" name="day" id="day"><br>
 
-form_field = "pic"
-form = cgi.FieldStorage()
-if 'pic' not in form:
-    text_out("Not yet submitted")
-else:
-    fileitem = form[form_field]
+        <label class="inst" for="time">Time:</label>
+        <input type="time" name="time"><br>
 
-if not fileitem.file: 
-    text_out("Bad form field name pic not file")
+        <label class="inst" for="meal">Meal:</label>
+        <input class="inst" list="meals" id="meal" name="meal">
+            <datalist id="meals">
+            <option value="Breakfast">
+            <option value="Lunch">
+            <option value="Supper">
+            <option value="Snack">
+            </datalist>
+        <br>
+        </fieldset>
+        <input type="submit"><br>
+        </form>
+        """
 
-if not fileitem.filename:
-    text_out("No filename given - did you choose a file?")
+    # Meal Selections should be changed to be based on table
+    # Meal selections could be radio buttons (perhaps too hard on phone
 
-fout = file(os.path.join(UPLOAD_DIR, fileitem.filename), 'wb')
+    def parse_form_fields(self):
+        self.get_form_fields()
+        if self.form.keys():
+            self.set_outfile_name()
+            self.handle_image()
+            self.handle_text()
+            self.text_out("Uploaded %s" % self.bname)
+        else:
+            self.text_out("Not yet submitted")
 
-while True:
-    chunk = fileitem.file.read(100000)
-    if not chunk: break
-    fout.write(chunk)
-fout.close()
+    def set_outfile_name(self):
+        """ Using timestamp as basis for filenames """
+        self.bname = datetime.now().strftime("%Y%m%dT%X")
 
-bname, dummy = os.path.splitext(fileitem.filename)
-fout = file(os.path.join(UPLOAD_DIR, bname + ".txt"), 'wb')
-fout.write(form.getfirst("Description", ""))
-fout.close()
+    FORM_FIELD = "pic"
+    def get_form_fields(self):
+        self.form = cgi.FieldStorage()
 
-text_out("File uploaded")
+    def handle_image(self):
+        if 'pic' not in self.form:
+            self.text_out("Not yet submitted")
+            return
+        self.fileitem = self.form[self.FORM_FIELD]
 
-#   Cal:<input type="number" name="calories" size="2" maxsize="4" max="3000">
-#   Carb:<input type="number" name="carbs" size="2" maxsize="3" max="300">
-#   Prot:<input type="number" name="protein" size="2" maxsize="3" max="300">
-#   Fat:<input type="number" name="fat" size="2" maxsize="3" max="300"><br>
-#  servings: <input type="range" name="servings" id="servingID" min="1" max="9" value="1" oninput="servingOutID.value=servingID.value">
-#  <output name="servingOutputName" id="servingOutID">1</output><br>
-#  Description: (used as title for dish)<br> <textarea name="description" rows="3" cols="35"> </textarea>
+        if not self.fileitem.file: 
+            self.text_out("Bad form field name pic not file")
+            return
+
+        if not self.fileitem.filename:
+            self.text_out("No filename given - did you choose a file?")
+            return
+
+        self.picfile_name = self.fileitem.filename
+
+        fout = file(os.path.join(UPLOAD_DIR, self.bname+".jpg"), 'wb')
+
+        while True:
+            chunk = self.fileitem.file.read(100000)
+            if not chunk: break
+            fout.write(chunk)
+        fout.close()
+
+    def handle_text(self):
+        UPLOAD_SECTION = "upload"
+        output = SafeConfigParser()
+        output.add_section(UPLOAD_SECTION)
+
+        data = dict((x,self.form.getfirst(x)) for x in self.form if x != self.FORM_FIELD)
+
+        for key in data.keys():
+            output.set(UPLOAD_SECTION, key, data[key])
+        output.set(UPLOAD_SECTION, "image_file", self.picfile_name or "")
+
+        output.add_section("edit")
+        for key in data.keys():
+            output.set("edit", key, data[key])
+
+        with open(os.path.join(UPLOAD_DIR, self.bname + ".ini"), 'wb') as outfile:
+            output.write(outfile)
+    
+
+if __name__ == '__main__':
+    main()
--- process.py
+++ process.py
@@ -1,32 +1,235 @@
 #!/usr/bin/python
 """
- This is going to be a program to process images uploaded, generally from an
- Android phone, which I am using to keep track of my diet.
+ Program to process information from an files uploaded from phone.
 
- There will be files in the base directory (for now it is www/and/images)
- that have been newly updated. There will be two files with matching names
- apart from extensions; a .txt and a .jpg (or possible png).
- I want to
+ This program is one of a group of programs to help me create and maintain a
+ record of everything I eat. This one normally runs as a crong job and copies
+ information from the images into the ini file that holds the text values, and
+ created a thumbnail (or at least smaller) image which is used for most of the
+ web pages.  The resulting pages are moved out of the staging directory into
+ archive or working directories.
 
  """
-import sys
+
+from glob import glob
 import os
-import tempfile
 import shutil
 from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+from ConfigParser import SafeConfigParser
+from datetime import datetime, time
 
 
-UPLOAD_DIR = "/big/dom/xkirkbird/www/and/images/"
-THUMB_DIR = UPLOAD_DIR + "thumbs/"
-ARCHIVE_DIR = UPLOAD_DIR + "archive/"
+UPLOAD_DIR = "/home/mbirdsall/food/upload/"
+THUMB_DIR = "/home/mbirdsall/food/thumbs/"
+ARCHIVE_DIR = "/home/mbirdsall/food/archive/"
+DATA_DIR = "/home/mbirdsall/food/byday"
+#UPLOAD_DIR = "/big/dom/xkirkbird/www/and/images/"
+#THUMB_DIR = UPLOAD_DIR + "thumbs/"
+#ARCHIVE_DIR = UPLOAD_DIR + "archive/"
 
 Thumb_size = 400, 300
 
-files = os.listdir(UPLOAD_DIR)
+class main():
+    def __init__(self):
+        self.picdaytime = ""
 
-files = [os.path.join(UPLOAD_DIR, file) for file in files 
-    if file.endswith(".jpg") or file.endswith(".png")]
+    def run(self):
+        os.chdir(UPLOAD_DIR)
 
+        if not os.path.exists(THUMB_DIR):
+            os.makedirs(THUMB_DIR)
+
+        if not os.path.exists(ARCHIVE_DIR):
+            os.makedirs(ARCHIVE_DIR)
+
+        if not os.path.exists(DATA_DIR):
+            os.makedirs(DATA_DIR)
+
+        for basename in self.get_basenames():
+
+            try:
+                ini = open(basename + ".ini", 'r+')
+            except IOError:
+                ini = None
+
+            try:
+                jpg = Image.open(basename + ".jpg")
+            except IOError:
+                jpg = None
+
+            try:
+                png = Image.open(basename + ".png")
+            except IOError:
+                png = None
+
+            self.process(basename, ini, jpg, png)
+
+
+    def get_basenames(self):
+        # There are files grouped by their basename without extension.
+        # Currently the basename is a timestamp when uploaded.
+        # There should be an .ini file and possibly either a jpg or png file
+        # But I may want to handle an image without an ini for recovery purposes
+        files = glob("*.ini") + glob("*.jpg") + glob("*.png")
+        return set(
+            os.path.splitext(
+                os.path.split(x)[1]
+                )[0] for x in files)
+
+    def read_ini_data(self, ini):
+        """ Get data out of the ini file """
+        self.ini_parser = SafeConfigParser()
+        self.ini_parser.readfp(ini)
+        if ('edit') not in self.ini_parser.sections():
+            self.ini_parser.add_section('edit')
+            for field in self.ini_parser.options('upload'):
+                if field == 'image_file':
+                    continue
+                self.ini_parser.set('edit', field,
+                    value=self.ini_parser.get('upload', field))
+
+    def read_exif_data(self, img):
+        self.picdaytime = None
+        if not img:
+            return
+        exif = img._getexif()
+        if not exif:
+            return
+        for tag, value in exif.items():
+            decoded = TAGS.get(tag, tag) # or get(tag, hex(tag))
+            # Put appropriate data into self
+            if decoded.startswith('DateTime') and not self.picdaytime:
+                # Transform 'YYYY:MM:DD HH:MM:SS' to datetime
+                self.picdaytime = datetime.strptime(value, "%Y:%m:%d %H:%M:%S")
+                # self.picdaytime = datetime(*map(int, value.replace(':', ' ').split()))
+            elif decoded == 'GPSInfo':
+                gps_data = dict()
+                for t in value:
+                    sub_decoded = GPSTAGS.get(t, t)
+                    gps_data[sub_decoded] = value[t]
+                self.gps_data = gps_data
+
+    def update_datetime(self, basename):
+        """ Use multiple possibilities to default date/time """
+
+        # datetime is set (in this priority) from the edit section of the ini,
+        # from the exif date and time in the image, or from the filename
+        # chosen at upload
+
+        saved = self.ini_parser
+        if saved.has_option('edit', 'day') and saved.get('edit', 'day'):
+            return
+
+        if saved.has_option('edit', 'time') and saved.get('edit', 'time'):
+            return
+
+        if self.picdaytime:
+            picday = self.picdaytime.date().strftime("%Y-%m-%d")
+            pictime = self.picdaytime.time().strftime("%H:%M")
+
+            saved.set('edit', 'day', value=picday)
+            saved.set('edit', 'time', value=pictime)
+            return
+
+        if not basename:
+            return
+
+        basename_datetime = datetime.strptime(basename, "%Y%m%dT%H:%M:%S")
+        basename_day = basename_datetime.date().strftime("%Y-%m-%d")
+        basename_time = basename_datetime.time().strftime("%H:%M")
+        saved.set('edit', 'day', value=basename_day)
+        saved.set('edit', 'time', value=basename_time)
+
+    def update_meal(self):
+        saved = self.ini_parser
+        if saved.has_option('edit', 'meal') and saved.get('edit', 'meal'):
+            return
+        if not saved.has_option('edit', 'time') or not saved.get('edit', 'time'):
+            return
+
+        mealtime = saved.get('edit', 'time')
+        mealtime = datetime.strptime(mealtime, "%H:%M").time()
+        if mealtime < time(4):
+            meal = "Snack"
+        elif mealtime < time(11):
+            meal = "Breakfast"
+        elif mealtime < time(15):
+            meal = "Lunch"
+        elif mealtime < time(17):
+            meal = "Snack"
+        elif mealtime < time(21, 30):
+            meal = "Supper"
+        else:
+            meal = "Snack"
+
+        saved.set('edit', 'meal', meal)
+
+
+    def create_empty_ini(self, basename):
+        """ Create an empty ini file and open as ini """
+        self.ini_parser = SafeConfigParser()
+        self.ini_parser.add_section('upload')
+        self.ini_parser.add_section('edit')
+        for field in ('description', 'comment', 'size', 'servings', 'calories',
+                'carbs', 'protein', 'fat', 'day', 'time', 'meal'):
+            self.ini_parser.set('upload', field, "")
+            self.ini_parser.set('edit', field, "")
+        return open(basename+".ini", mode='w+')
+
+    def process(self, basename, ini, jpg, png):
+        """ Fill in, thumbnail, and move out the way """
+
+
+        print "Processing %s" % basename
+
+        # Shouldn't have both jpg and png, but give precedence to jpg
+        img = jpg or png
+
+        if jpg and png:
+            shutil.move(png.filename,
+                os.path.join(ARCHIVE_DIR, basename+'.png'))
+
+        if not ini:
+            ini = self.create_empty_ini(basename)
+        else:
+            self.read_ini_data(ini)
+
+        # If not already set, day and time can come from the image file
+        # But if they were uploaded or edited already, don't use it
+        # GPS and location will be the same way when they get done
+        self.read_exif_data(img)
+        self.update_datetime(basename)
+        self.update_meal()
+        ini.seek(0,0)
+        self.ini_parser.write(ini)
+
+        # Create the thumbnail
+        if img:
+            thumbfile_name = os.path.join(THUMB_DIR, basename+".jpg")
+            thumbfile = open(thumbfile_name, mode="w+")
+            thumb = img.copy()
+            thumb.thumbnail(Thumb_size, Image.ANTIALIAS)
+            thumb.save(thumbfile_name, "JPEG")
+            thumbfile.close()
+            os.chmod(thumbfile_name, 0644)
+            shutil.move(img.filename, os.path.join(ARCHIVE_DIR, img.filename))
+
+        ini_dest = os.path.join(DATA_DIR, ini.name)
+        shutil.move(ini.name, ini_dest)
+        # Now put a link to the same file in a directory for the day
+        day_dir = os.path.join(DATA_DIR, self.ini_parser.get('edit', 'day'))
+        if not os.path.exists(day_dir):
+            os.makedirs(day_dir)
+        os.link(ini_dest, os.path.join(day_dir, ini.name))
+
+        ini.close()
+
+
+
+
+
+'''
 for fname in files:
 
     # Intention is to make this rerunnable, so original files are
@@ -40,6 +243,9 @@ for fname in files:
     newfile = tempfile.NamedTemporaryFile(suffix='.jpg', prefix='image_',
         dir=THUMB_DIR, delete=False)
 
+'''
+
+'''
     # Save thumbnail copy
     thumb = image.copy()
     thumb.thumbnail(Thumb_size, Image.ANTIALIAS)
@@ -65,6 +271,9 @@ for fname in files:
         #   newfile.name
 
 
+'''
+
+'''
     path, basename = os.path.split(fname)
     basename, ext = os.path.splitext(basename)
     textname = os.extsep.join([basename, "txt"])
@@ -73,10 +282,8 @@ for fname in files:
     shutil.move(fname, os.path.join(ARCHIVE_DIR, basename+ext))
 
     textfull = os.path.join(UPLOAD_DIR, textname)
-    """
-    if os.path.isfile(textname):
-        shutil.move(textfull, os.path.join(ARCHIVE_DIR, textname))
-    """
+    #if os.path.isfile(textname):
+        #shutil.move(textfull, os.path.join(ARCHIVE_DIR, textname))
 
     try:
         shutil.move(textfull, os.path.join(ARCHIVE_DIR, textname))
@@ -88,6 +295,11 @@ for fname in files:
 
 
     # TODO: Deal with duplicate and missing filenames
+
+'''
+
+if __name__ == '__main__':
+    main().run()
 
     """
     Move the data from the .txt file into a databaes (perhaps ultimately, they
