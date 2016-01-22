5577ac3ab4af0f48363423f19160744662b88d06
mbirdsall <devnull@localhost>
2016-01-22 18:50:56 +0000

    Added creating a list

---
+++ list_form.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+""" Create html page with full listing of items in my food record
+
+"""
+
+import os, sys
+from glob import glob
+from ConfigParser import SafeConfigParser
+from collections import defaultdict, namedtuple
+from operator import attrgetter
+
+ITEM = namedtuple('item',
+    'comment carbs description servings calories fat day time '
+    'protein meal size')
+
+#UPLOAD_DIR = "/big/dom/xkirkbird/www/and/images/"
+#THUMB_DIR = UPLOAD_DIR + "thumbs/"
+#ARCHIVE_DIR = UPLOAD_DIR + "archive/"
+#DATA_DIR = UPLOAD_DIR + "byday/"
+UPLOAD_DIR = "/home/mbirdsall/food/upload/"
+THUMB_DIR = "/home/mbirdsall/food/thumbs/"
+ARCHIVE_DIR = "/home/mbirdsall/food/archive/"
+DATA_DIR = "/home/mbirdsall/food/byday"
+
+SECTION = 'edit'
+
+
+def main():
+    """ Generate full food listing html """
+
+    # Gather all the info
+    os.chdir(DATA_DIR)
+
+    files = glob("*.ini")
+
+    items = dict() # all fields for each item identified by upload second
+    days = defaultdict(list) # list of all items in a day
+
+    for _file in files:
+        upload_second = os.path.splitext(_file)[0]
+        parser = SafeConfigParser()
+        parser.read(_file)
+        info = dict(parser.items(SECTION))
+        fitem = ITEM(**info)
+        items[upload_second] = fitem
+        days[fitem.day].append(upload_second)
+
+    """
+    for day, item_list in sorted(days.iteritems()):
+        for item in sorted(item_list):
+            print day, items[item].time, items[item].meal, items[item].description
+    """
+
+    print_header()
+    print_body_start()
+    for day, item_list in sorted(days.iteritems()):
+        print_dayrow(day)
+        print_item_rows([value for key, value in items.items() if key in item_list])
+    print_afterward()
+
+    sys.exit()
+
+def sorted_items_by_meal(item_rows):
+    meals = defaultdict(list)
+    for item in item_rows:
+        meals[item.meal].append(item)
+
+    for meal in meals.values():
+        meal.sort(key=attrgetter('time'))
+
+    z = sorted(meals.values(), key=lambda x: x[0].time)
+    return z
+
+
+def ellipse_truncate(s, length):
+    if s:
+        return (s[:length] + "&hellip;") if len(s) > length+2 else s
+    else:
+        return "No Description Yet"
+
+def print_meal(meal):
+    width = 40
+    desc = ellipse_truncate(meal[0].description, width)
+    print """<tr>
+    <th rowspan="%s">%s</th>
+    <td>%s</td>
+    </tr>""" % (len(meal), meal[0].meal, desc)
+    for dish in meal[1:]:
+        desc = ellipse_truncate(dish.description, width)
+        print """<tr><td>%s</td></tr>""" % desc
+
+def print_item_rows(item_rows):
+    """ Divide by meal """
+    # Get the meals and order them by earliest item
+
+    meals = sorted_items_by_meal(item_rows)
+    for meal in meals:
+        print_meal(meal)
+
+def print_afterward():
+    print """</table>
+    </body>
+    </html>"""
+
+def print_header():
+    print """<html>
+    <head>
+      <title>Food Items</title>
+        <style>
+          table {
+            background:#fff7db;
+          }
+          table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+          }
+          th, td {
+            padding: 5px;
+            white-space: nowrap;
+          }
+        </style>
+      </head>
+    """
+
+def print_body_start():
+    print """<body>
+    <h1>MGB Food Log</h1>
+    <table>
+    """
+
+def print_dayrow(day):
+    print """<tr>
+      <th colspan="2">%s</th>
+      </tr>
+      <tr>
+        <th>Meal</th>
+        <th>Item</th>
+""" % day
+
+def print_first_item_row(item_rows):
+    print """<tr>
+    <th rowspan="%s">%s</th>
+      <td>%s</td>
+    </th>""" % (len(item_rows), item_rows, item_rows)
+
+if __name__ == '__main__':
+    main()
--- process.py
+++ process.py
@@ -20,13 +20,14 @@ from ConfigParser import SafeConfigParser
 from datetime import datetime, time
 
 
-UPLOAD_DIR = "/home/mbirdsall/food/upload/"
-THUMB_DIR = "/home/mbirdsall/food/thumbs/"
-ARCHIVE_DIR = "/home/mbirdsall/food/archive/"
-DATA_DIR = "/home/mbirdsall/food/byday"
-#UPLOAD_DIR = "/big/dom/xkirkbird/www/and/images/"
-#THUMB_DIR = UPLOAD_DIR + "thumbs/"
-#ARCHIVE_DIR = UPLOAD_DIR + "archive/"
+#UPLOAD_DIR = "/home/mbirdsall/food/upload/"
+#THUMB_DIR = "/home/mbirdsall/food/thumbs/"
+#ARCHIVE_DIR = "/home/mbirdsall/food/archive/"
+#DATA_DIR = "/home/mbirdsall/food/byday"
+UPLOAD_DIR = "/big/dom/xkirkbird/www/and/images/"
+THUMB_DIR = UPLOAD_DIR + "thumbs/"
+ARCHIVE_DIR = UPLOAD_DIR + "archive/"
+DATA_DIR = UPLOAD_DIR + "byday/"
 
 Thumb_size = 400, 300
 
@@ -300,81 +301,4 @@ for fname in files:
 
 if __name__ == '__main__':
     main().run()
-
-    """
-    Move the data from the .txt file into a databaes (perhaps ultimately, they
-    should be put in the database when uploading), create a thumbnail from the
-    .jpg and put it in a thumbnail directory, extract exif information from the
-    .jpg into the database and move the .jpg to an archive, # which indicates
-    that the processing is done.
-
-    The exif processing and the thumbnail processing can all be done in python
-    with the PIL (Python Imaging Library).
-
-    To create the thumbnail, open the image with
-
-        from PIL Import Image
-
-        newsize = width, height
-        im = Image(infile)
-        tn = im.copy()
-        tn.thumbnail(newsize, Image.ANTIALIAS)
-        tn.save(outputpath, 'JPEG')
-
-        can also use tn = im.resize(newsize, Image.ANTIALIAS) instead. Check
-        the implications of this.  In either case, have to deal with aspect
-        ratio.
-
-
-    To deal with exif values
-    (Also check on using the piexif library with PIL)
-
-        from PIL.ExifTags import TAGS
-
-        img = Image.open(imgdir + fname)
-        exif = img._getexif()
-        if exif:
-            for tag, value in exif.items():
-                decoded = TAGS.get(tag, tag) # or get(tag, hex(tag))
-                if decoded == 'Orientation':
-                    if value == 3: img = img.rotate(180)
-                    ...
-
-    Dealing with GPS Latitude and Longitude
-
-        from PIL.ExifTags import TAGS, GPSTAGS
-
-        im = Image(infile)
-        info = im._getexif()
-        if info:
-            for tag, value in info.items():
-                decoded = get(tag, tag)
-                if decoded == 'GPSInfo':
-                    gps_data = dict()
-                    for t in value:
-                        sub_decoded = GPSTAGS.get(t, t)
-                        gps_data[sub_decoded] = value[t]
-
-                    exif_data[decoded] = gps_value
-                else:
-                    exif_data[decoded] = value
-
-        # exif_data['GPSInfo']['GPSLatitude'] and
-        # exif_data['GPSInfo']['GPSLongitude']
-        # each have a three element array with each holding two
-
-    Dealing with time picture taken (in Exif data)
-
-    Thumbnail in IFD1 of EXIF data?
-
-    ://pypi.python.org/pypi/ExifRead
-
-
-    resources:
-    http://andrius.miasnikovas.lt/2010/04/creating-thumbnails-from-photos-with-python-pil/
-    https://opensource.com/life/15/2/resize-images-python
-    http://effbot.org/imagingbook/image.htm
-    http://eran.sandler.co.il/2011/05/20/extract-gps-latitude-and-longitude-data-from-exif-using-python-imaging-library-pil/comment-page-1/
-    """
-
 
