c1f93236dba22c637365d9b05b27262347601e8a
Mike Birdsall <m.g.birdsall@mbirdsall.com>
2016-01-23 20:12:58 -0500

    Merged from mgb container - different my_info.py for different machines

--- my_info.py
+++ my_info.py
@@ -1,10 +1,6 @@
-
-ROOT_DIR = "/home/mbirdsall/food/"
-#ROOT_DIR = "/big/dom/xkirkbird/"
-
+ROOT_DIR = "/big/dom/xkirkbird/"
 DATA_DIR = ROOT_DIR + "www/and/images/byday/"
 THUMB_DIR = ROOT_DIR + "www/and/images/thumbs"
 THUMB_URL = "/and/images/thumbs/"
-
 UPLOAD_DIR = ROOT_DIR + "www/and/images"
 ARCHIVE_DIR = UPLOAD_DIR + "archive/"
