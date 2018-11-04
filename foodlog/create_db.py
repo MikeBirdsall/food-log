45e23955f20899d2353e3f92d6d3647f35d60ad1
MikeBirdsall <8390029+MikeBirdsall@users.noreply.github.com>
2018-11-04 07:46:53 -0500

    Remove remnants of obsolete .ini file style courses
    An earlier version of the code didn't use a database, but kept entries
    in .ini files. There are still remnants in that in the exectutable to
    create the sqlite3 database file. Remove the last vestiges of that.
    
    Fixes: #8

--- foodlog/create_db.py
+++ foodlog/create_db.py
@@ -9,9 +9,7 @@
 
 """
 import os
-from glob import glob
 import argparse
-from ConfigParser import SafeConfigParser
 import sqlite3
 from my_info import config_path
 
@@ -21,64 +19,22 @@ class ConstructDatabase(object):
         config = config_path()
         sqlite_file = args.sqlite_file or config.dir("DB_FILE")
         log_file = args.log_file or config.dir("DB_LOG")
-        self.default_ini_files = list((config.dir("DATA_DIR"), config.dir("TEMPLATE_DIR")))
 
         if os.path.exists(sqlite_file):
             open(sqlite_file, 'w').close()
         self.conn = sqlite3.connect(sqlite_file)
         self.cursor = self.conn.cursor()
         self.log_file = open(log_file, 'w')
-        self.dir_paths = None
 
-    def process(self, dir_paths):
-        self.dir_paths = dir_paths or self.default_ini_files
+    def process(self):
         self.create_tables()
-        self.populate_tables()
         self.conn.commit()
         self.conn.close()
 
-    def populate_tables(self):
-        files = list()
-        for path in self.dir_paths:
-            files.extend(glob(os.path.join(path, "*.ini")))
-
-        for file_ in files:
-            self.insert_in_db(self.extract_from_ini(file_))
-
-    def insert_in_db(self, record):
-        """ Insert database row build from dict """
-        kind, dict_ = record
-        fields = [x for x in dict_ if dict_[x] != ""]
-        if fields:
-            vals = [dict_[x] for x in fields]
-            line = "INSERT INTO %s (%s) VALUES(%s)" % (
-                kind, ", ".join(fields), ", ".join("?" for x in fields))
-            self.cursor.execute(line, vals)
-            print >> self.log_file, dict(command=line, args=vals)
-        else:
-            raise RuntimeError('no fields in record: %s', dict_)
-
-    def extract_from_ini(self, file_):
-        """ Return dict of field values from ini file """
-        parser = SafeConfigParser()
-        parser.read(file_)
-        if parser.has_section('upload'):
-            combined = dict(("orig_"+x[0], x[1])
-                for x in parser.items("upload"))
-            combined['image_file'] = combined.pop('orig_image_file')
-            combined.update(dict(parser.items("edit")))
-            combined['ini_id'] = combined.pop('id')
-            return 'course', combined
-
-        elif parser.has_section('template'):
-            combined = dict(parser.items('template'))
-            return 'template', combined
-
-
     def create_tables(self):
         """ Create tables from schema in file tables.sql """
-        with open('tables.sql', 'r') as f:
-            schema = f.read()
+        with open('tables.sql', 'r') as sqlite_file:
+            schema = sqlite_file.read()
         self.conn.executescript(schema)
         print >> self.log_file, schema
 
@@ -89,7 +45,7 @@ def main():
     parser.add_argument("--sqlite_file", '-d', type=str, help="output database file")
     parser.add_argument("--log_file", '-l', type=str, help="output sql log file")
     args = parser.parse_args()
-    ConstructDatabase(args).process(args.input_paths)
+    ConstructDatabase(args).process()
 
 if __name__ == '__main__':
     main()
